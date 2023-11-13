9e12974c2d7be6ce7aedb1e26d75b667a0e52e81
mahdi mohamed <mahdimoham3412@gmail.com>
2023-11-13 12:51:25 +0000

    First commit

---
+++ data_extraction.py
@@ -0,0 +1,13 @@
+from database_utils import *
+
+
+class DataExcractor(DatabaseConnector):
+    
+    def __init__(self):
+        pass
+
+    def list_db_tables(init_db_engine):
+        pass
+
+    def read_rds_table():
+        pass
---
+++ database_utils.py
@@ -0,0 +1,14 @@
+import yaml
+
+class DatabaseConnector():
+    
+    def __init__(self):
+        pass
+
+    def read_db_creds(self):
+        pass
+
+    def init_db_engine(read_db_creds):
+        pass
+
+    
